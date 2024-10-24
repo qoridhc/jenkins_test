16f07a949091a375970d56a6cbd549122a62ea15
이성현 <vkse1934@naver.com>
2024-10-24 23:18:01 +0900

    Feat: 초기 프로젝트 생성

---
+++ travelus/src/main/java/com/example/travelus/TravelusApplication.java
@@ -0,0 +1,13 @@
+package com.example.travelus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TravelusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TravelusApplication.class, args);
+	}
+
+}
---
+++ travelus/src/test/java/com/example/travelus/TravelusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.travelus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TravelusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
